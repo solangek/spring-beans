5a14419337e591f32aaae4da495438c17c278c89
Sol <sodanabe@gmail.com>
2023-05-07 17:06:06 +0300

    Initial commit

---
+++ src/main/java/hac/springbeans/SpringBeansApplication.java
@@ -0,0 +1,13 @@
+package hac.springbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hac/springbeans/SpringBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package hac.springbeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
